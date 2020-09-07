f11fc6b2763c10159e264d2f6010f7e3c65462a3
zainab <young8femme@gamil.com>
2020-09-08 01:22:48 +0300

    added results area in HTML, CSS and Js

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ function computerPlay(){
 
 function round(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-
     switch (playerSelection){
         case 'rock':
             if(computerSelection == 'paper'){
+                trackComputer++;
                 return "You Lose! Paper beats Rock";
             }
             else if(computerSelection == 'scissors'){
+                trackPlayer++;
                 return "You Win! Rock beats Scissors";
             }
             else{
@@ -21,9 +22,11 @@ function round(playerSelection, computerSelection){
             break;
         case 'paper':
             if(computerSelection == 'scissors'){
+                trackComputer++;
                 return "You Lose! Scissors beats Paper";
             }
             else if(computerSelection == 'rock'){
+                trackPlayer++;
                 return "You Win! Paper beats Rock";
             }
             else{
@@ -32,24 +35,44 @@ function round(playerSelection, computerSelection){
             break;
         case 'scissors':
                 if(computerSelection == 'rock'){
+                    trackComputer++;
                     return "You Lose! Rock beats Scissors";
                 }
                 else if(computerSelection == 'Paper'){
+                    trackPlayer++;
                     return "You Win! Scissors beats Paper";
                 }
                 else{
                     return "Tie!";
                 }
                 break;
+        default:
+            return 'blank';
     }
 }
 
-function game(){
-    for(let i=0; i<5; i++){
-        computer = computerPlay();
-        // player = window.prompt('Choose Rock, Paper or Scissors: ');
-        console.log(round(player, computer));
-    }
+function game(player){
+    computer = computerPlay();
+    document.querySelector('.dynamic-text').textContent=round(player, computer);
+    
+    playerResult.textContent = ''+trackPlayer;
+    computerResult.textContent = ''+trackComputer;
+
 }
 
-game();
+let trackComputer = 0;
+let trackPlayer = 0;
+
+let choices = document.querySelectorAll('.player-choice');
+let playerResult = document.querySelector('.dynamic-result-player');
+let computerResult = document.querySelector('.dynamic-result-computer');
+
+playerResult.textContent = ''+trackPlayer;
+computerResult.textContent = ''+trackComputer;
+
+choices.forEach(choice => {
+    choice.addEventListener('click', ()=>{
+        
+        game(choice.classList[1]);
+    })
+})
